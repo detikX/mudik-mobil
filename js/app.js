ad19bc332f8bad2671d8dab639de2e585b232c5d
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-03-20 09:54:16 +0700

    dcdc

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    AOS.init();
+    // new WOW().init();
+
+    // setTimeout(() => {
+    //     $(".preloader").slideUp(1000);
+    //     $(".preloader").remove();
+
+    // }, 1500);
+    // new WOW().init();
+})
---
+++ js/gsap-stick.js
@@ -0,0 +1,42 @@
+// gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".box2",
+//         start: "center center",
+//         end: "bottom top",
+//         scrub: 1,
+//         pin: true
+//     }
+// })
+//     .from(".box2", {
+//         opacity: 0,
+//     })
+//     .from(".text4", {
+//         y: innerHeight * 1,
+//         stagger: {
+//             amount: 0.5,
+//         }
+//     })  
+
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".box",
+        start: "center center",
+        end: "bottom top",
+        scrub: 1,
+        pin: true
+    }
+})
+    .from(".logo", {
+        y: innerHeight * 1,
+        rotate: 360
+    })
+    .from(".text1", {
+        x: innerWidth * 1
+    })
+    .from(".text2", {
+        x: innerWidth * -1
+    })
+    .from(".text3", {
+        x: innerWidth * 1
+    })

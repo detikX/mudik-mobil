75e74ce1fac50c7099862ad42eaccc62d2c7cf2d
dediwibisono17 <dedi.wibisono17@gmail.com>
2025-03-23 05:44:29 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -8,4 +8,30 @@ $(document).ready(function () {
 
     // }, 1500);
     // new WOW().init();
+
+    //accordion
+
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title,.minus').click(function (e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if ($(e.target).is('.active')) {
+            close_accordion_section();
+        } else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
 })
+
